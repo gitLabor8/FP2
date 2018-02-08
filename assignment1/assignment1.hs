8705f98f1150fe6d15f16c2637007c99ab1ab772
mavala97 <max.van@hotmail.com>
2018-02-08 16:49:40 +0100

    1.2 done

--- assignment1/assignment1.hs
+++ assignment1/assignment1.hs
@@ -1,6 +1,9 @@
 import Data.List
+import Tree
 
-main = print (frequencyTable why)
+main = print (build (frequencyTable ezExample))
+
+ezExample = "AAABBC"
 
 why =
    "As software becomes more and more complex, it\n\
@@ -31,6 +34,9 @@ instance (Eq a) => Eq (With a b) where
   (a :- _) == (b :- _)  =  a == b
 instance (Ord a) => Ord (With a b) where
   (a :- _) <= (b :- _)  =  a <= b
+sndWith :: (With a b) -> b
+sndWith (a :- b) = b
+
 -- # Exercise 1.1
 
 frequencyTable :: (Ord char) => [char] -> [With Int char]
@@ -48,13 +54,15 @@ a = Frequency
 b = letter
 c = With a b
 --}
-{--
-# Exercise 1.2
-/begin haskell comment
+
+-- Exercise 1.2
+
+--data Tree a = (:^:) (Tree a) (Tree a)
+--       | Leaf a
+
+{--/begin haskell comment
 Tree a = Node (Tree a) (Tree a)
-       | Leaf a
-Tree a = (:^:) (Tree a) (Tree a)
-       | Leaf a
+| Leaf a
 
 case Tree a of
   Node t1 t2 ->
@@ -79,11 +87,26 @@ buildBalanced as  = build (take n as) :^: build (drop n as)
   where n = length as ('div' 2)
 
 /einde haskell comment
-
-* sort [with a b]
-*
-build :: [With a b] -> [with a (Tree b)]
-
-
-*
 --}
+
+
+build :: [With Int b] -> Tree b
+build = build' . map (\ (freq :- letter) -> freq :- Leaf letter) . sort
+  where
+--      build' :: [With a (Tree b)] -> Tree b
+      build' [a :- tree] =  tree
+      build' (a:b:xs) =
+        let
+          ab (freqA :- treeA) (freqB :- treeB)  = (freqA + freqB) :- (treeA :^: treeB)
+        in
+          build' (insert (ab a b) xs)
+
+  -- let
+  --   sortedFrequencyTable = sort frequencyTable
+  --   singleTrees =  map (\ (freq :- letter) -> freq :- Leaf letter) sortedFrequencyTable
+  --
+  --   a:b:xs = singleTrees
+  --   ab (With freqA treeA) (With freqB treeB)  = With (freqA + freqB)(Node treeA treeB)
+  --   -- Maybe a bit inefficient. Oh well...
+  --   sort ((ab a b):xs)
+  -- in
